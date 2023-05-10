8d58e51c7f001d7ee0f7bfdb7a62d5a9d06e6c48
Sreesanth46 <sreesanth46@gmail.com>
2023-05-10 10:00:10 +0530

    feat: Login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,1 +1,46 @@
+import { useState } from "react";
+import { AppInput } from "./AppInput";
+import AgoraRTM from "agora-rtm-sdk";
 
+let options = {
+    uid: "",
+    token: "",
+};
+
+// Your app ID
+const appID = "765b5fbbb5f4496cbdf8128646dd8fbd";
+// Your token
+options.token = "<Your token>";
+
+export const Login = () => {
+    const [formValues, setFormValues] = useState({
+        first: "",
+        second: "",
+    });
+
+    const onChange = (value: string, inputName: string) => {
+        setFormValues({ ...formValues, [inputName]: value });
+    };
+
+    const logForm = () => {
+        console.log(formValues);
+    };
+
+    return (
+        <>
+            <AppInput
+                inputName="first"
+                label="User ID"
+                value={formValues.first}
+                onChange={onChange}
+            ></AppInput>
+            <AppInput
+                inputName="second"
+                label="User Name"
+                value={formValues.second}
+                onChange={onChange}
+            ></AppInput>
+            <button onClick={logForm}>Show</button>
+        </>
+    );
+};
